f2afe0cb78d00a4dbd6eb0e83be1d0ff5959cdb8
JoaoArthur-bat <joao_arthur03@outlook.com>
2026-04-21 18:21:56 -0300

    Colocando um Extrato

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const botaoAdicionar = document.querySelector('button');
 const inputDescricao = document.querySelector('input[type="text"]');
 const inputValor = document.querySelector('input[type="number"]');
 const displaySaldo = document.querySelector('#balance h2');
-
 const selectTipo = document.querySelector('#tipo-transacao');
+
+const listaTransacoes = document.querySelector('#lista-transacoes');
 
 let saldoSalvo = localStorage.getItem('saldoUsuario');
 let saldoTotal = saldoSalvo ? parseFloat(saldoSalvo) : 0;
@@ -15,13 +16,15 @@ displaySaldo.innerHTML = `Saldo Atual: R$ ${saldoTotal.toFixed(2)}`;
 function atualizarSaldo() {
     const descricao = inputDescricao.value;
     const valorDigitado = parseFloat(inputValor.value);
+    
+    const tipo = selectTipo.value; 
 
     if(descricao === "" || isNaN(valorDigitado)) {
         alert("Por favor, preencha a descricao e o valor corretamente!");
         return;
     }
 
-    if (selectTipo.value === "saida") {
+    if (tipo === "saida") {
         saldoTotal -= valorDigitado;
     } else {
         saldoTotal += valorDigitado;
@@ -30,6 +33,17 @@ function atualizarSaldo() {
     displaySaldo.innerHTML = `Saldo Atual: R$ ${saldoTotal.toFixed(2)}`;
     localStorage.setItem('saldoUsuario', saldoTotal);
 
+    const novoItem = document.createElement('li'); 
+    novoItem.innerHTML = `<span>${descricao}</span> <span>R$ ${valorDigitado.toFixed(2)}</span>`;
+
+    if (tipo === "entrada") {
+        novoItem.classList.add('item-entrada');
+    } else {
+        novoItem.classList.add('item-saida');
+    }
+
+    listaTransacoes.appendChild(novoItem);
+   
     inputDescricao.value = "";
     inputValor.value = "";
     inputDescricao.focus();
